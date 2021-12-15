c695a6bc1663a7dea1b985cf9deebb0c9069e6dc
noe <csalazar089@gmail.com>
2021-12-15 13:11:16 -0600

    Agregando el nuevo hook

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -7,7 +7,6 @@ interface useProductArgs{
     value?:number;
 }
 
-<<<<<<< HEAD
 export const useProduct = ({onChange,product,value=0}:useProductArgs) => {
 
     const [ counter, setCounter ] = useState(value);
@@ -31,36 +30,6 @@ export const useProduct = ({onChange,product,value=0}:useProductArgs) => {
         setCounter(value);
 
     }, [value])
-=======
-interface useProductArgs {
-    product: Product;
-    onChange?: ( args: onChangeArgs ) => void;
-    value?: number;
-}
-
-
-export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
-
-    const [ counter, setCounter ] = useState( value );
-
-    const isControlled = useRef( !!onChange )
-
-    const increaseBy = ( value: number ) => {
-
-        if( isControlled.current ) {
-            return onChange!({ count: value, product });
-        }
-        
-        const newValue = Math.max( counter + value, 0 )
-        setCounter( newValue );
-
-        onChange && onChange({ count: newValue, product });
-    }
-
-    useEffect(() => {
-        setCounter( value );
-    }, [ value ])
->>>>>>> c1d06e5fe525f48ecbe29092cd63c566fd3e7329
 
     return {
         counter,
